a36e13a51761ca9ac72b5b1c3f92a49712aa5c08
pavel-mazhuga <pavelmazhuga@yandex.ru>
2019-02-23 13:07:58 +0700

    fix app config

--- app.config.js
+++ app.config.js
@@ -1,9 +1,11 @@
 /* eslint max-len: "off" */
 module.exports = {
+    LANGUAGE: 'ru',
     PROJECT_NAME: 'PROJECT_NAME',
     TITLE: 'YOUR_SITE_NAME',
     DESCRIPTION: 'DESCRIPTION',
-    USE_FAVICONS: true,
-    LANGUAGE: 'ru',
     HTML_PRETTY: true,
+    USE_FAVICONS: true,
+    USE_COMPRESSION: false,
+    USE_SERVICE_WORKER: false,
 };
