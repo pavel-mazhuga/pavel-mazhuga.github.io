e5e165904d866a4ee0c5ecee5d37bdcc0bada3de
pavel-mazhuga <pavelmazhuga@yandex.ru>
2019-03-31 23:12:10 +0700

    add jest

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -3,6 +3,7 @@ import { isPhone } from '../src/js/modules/validator';
 test('validates phone correctly', () => {
     expect(isPhone('89999999999')).toBe(true);
     expect(isPhone('+79999999999')).toBe(true);
+    expect(isPhone('+7 (999) 999-99-99')).toBe(true);
     expect(isPhone('+7999999')).toBe(true);
     expect(isPhone('')).toBe(false);
     expect(isPhone('assd')).toBe(false);
