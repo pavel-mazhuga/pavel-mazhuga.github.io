43a50251a9df355ad275bbb038326933942f223a
pavel-mazhuga <pavelmazhuga@yandex.ru>
2019-11-04 11:39:42 +0700

    smart subsetting

--- fonts.subsetting.bat
+++
@@ -1,8 +0,0 @@
-set SRC_FORMAT="ttf"
-set SRC_FORMAT=%SRC_FORMAT:"=%
-
-set FILENAME="HouschkaPro-Medium"
-set FILENAME=%FILENAME:"=%
-call pyftsubset src\fonts\src\%FILENAME%.%SRC_FORMAT% --output-file=src\fonts\%FILENAME%.ttf --unicodes-file=fonts-subsets.txt
-call pyftsubset src\fonts\src\%FILENAME%.%SRC_FORMAT% --output-file=src\fonts\%FILENAME%.woff --flavor=woff --unicodes-file=fonts-subsets.txt
-call pyftsubset src\fonts\src\%FILENAME%.%SRC_FORMAT% --output-file=src\fonts\%FILENAME%.woff2 --flavor=woff2 --unicodes-file=fonts-subsets.txt
