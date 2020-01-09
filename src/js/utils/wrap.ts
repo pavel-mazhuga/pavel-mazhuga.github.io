40b02b48ab36f9e4ae44a4a3eacc723e4aea0af8
Pavel Mazhuga <Pavel@iMac-Pavel.local>
2020-01-09 16:45:50 +0700

    wrap: add docs

--- src/js/utils/wrap.ts
+++ src/js/utils/wrap.ts
@@ -2,6 +2,15 @@ interface WrapOptions {
     class?: string;
 }
 
+/**
+ * Оборачивает элемент в другой элемент.
+ * Аналог метода "wrap" в jQuery.
+ *
+ * @param  {Element} el - DOM-элемент
+ * @param  {string} wrapNodeType - например, 'div'
+ * @param  {WrapOptions} options
+ * @returns Element
+ */
 export default (el: Element, wrapNodeType = 'div', options: WrapOptions = {}): Element => {
     const parent = el.parentNode;
     const wrapper = document.createElement(wrapNodeType);
