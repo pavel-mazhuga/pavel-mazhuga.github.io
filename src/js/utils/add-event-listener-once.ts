49b7ff500ef5a40d29fd6c3c7601556435b8f632
Pavel Mazhuga <Pavel@iMac-Pavel.local>
2020-01-09 16:42:20 +0700

    add-event-listener-once: add docs

--- src/js/utils/add-event-listener-once.ts
+++ src/js/utils/add-event-listener-once.ts
@@ -1,6 +1,14 @@
-export default (element: Element, eventName: string, fn: Function) => {
+/**
+ * Слушает событие только до первого триггера.
+ * Аналог метода "one" в jQuery.
+ *
+ * @param  {Element | Document | Window} element - DOM-элемент, который слушаем
+ * @param  {string} eventName - название события
+ * @param  {Function} callback
+ */
+export default (element: Element | Document | Window, eventName: string, callback: Function) => {
     element.addEventListener(eventName, function listener() {
         element.removeEventListener(eventName, listener);
-        fn();
+        callback();
     });
 };
