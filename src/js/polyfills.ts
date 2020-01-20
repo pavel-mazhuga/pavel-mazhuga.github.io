8419546a667d95dbf5b4de2ca7a3937e62be28b7
pavel-mazhuga <pavelmazhuga@yandex.ru>
2020-01-20 23:41:20 +0700

    move polyfills

--- src/js/polyfills.ts
+++ src/js/polyfills.ts
@@ -1,14 +1,14 @@
 /* global BUILD_TYPE */
 /* eslint global-require: "off" */
 import 'focus-visible';
-import 'element-matches-polyfill';
 // import 'intersection-observer';
 
 if (BUILD_TYPE === 'legacy') {
-    require('whatwg-fetch');
     require('core-js/es/symbol');
     require('core-js/es/promise/finally');
     // require('core-js/es/map');
     // require('core-js/es/set');
+    require('element-matches-polyfill');
     // require('picturefill');
+    require('whatwg-fetch');
 }
