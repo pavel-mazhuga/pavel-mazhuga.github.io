dac58dde3a7dbd8f05859fa0fb10b63031d6da28
pavel-mazhuga <pavelmazhuga@yandex.ru>
2021-11-27 16:48:47 +0700

    .

--- next.config.js
+++ next.config.js
@@ -8,4 +8,6 @@ module.exports = {
         // !! WARN !!
         ignoreBuildErrors: true,
     },
+    // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
