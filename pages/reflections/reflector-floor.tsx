13501ccc17f686f5af0bd4361c81e7a728bdd1de
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2021-12-03 16:23:13 +0700

    .

--- pages/reflections/reflector-floor.tsx
+++ pages/reflections/reflector-floor.tsx
@@ -1,20 +1,17 @@
 import * as THREE from 'three';
-import { useState, useRef, Suspense, useMemo } from 'react';
+import { useRef, Suspense, useMemo, MutableRefObject, useEffect } from 'react';
 import { Canvas, useThree, useFrame, useLoader } from '@react-three/fiber';
 import { Reflector, CameraShake, OrbitControls, useTexture } from '@react-three/drei';
-// import { KernelSize } from 'postprocessing';
-import { EffectComposer, Bloom } from '@react-three/postprocessing';
+import { EffectComposer, SMAA, SelectiveBloom, Bloom } from '@react-three/postprocessing';
+import { BlurPass, Resizer, KernelSize } from 'postprocessing';
 import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
 import DefaultLayout from 'components/layout/DefaultLayout';
 
-function Triangle({ color, ...props }) {
-    const ref = useRef();
-    const [r] = useState(() => Math.random() * 10000);
-    useFrame((_) => (ref.current.position.y = -1.75 + Math.sin(_.clock.elapsedTime + r) / 10));
-    const { paths: [path] } = useLoader(SVGLoader, '/triangle.svg') // prettier-ignore
+function ChipsaLogo({ color, ...props }) {
+    const { paths: [path] } = useLoader(SVGLoader, '/chipsa-logo.svg') // prettier-ignore
     const geom = useMemo(() => SVGLoader.pointsToStroke(path.subPaths[0].getPoints(), path.userData.style), []);
     return (
-        <group ref={ref}>
+        <group>
             <mesh geometry={geom} {...props}>
                 <meshBasicMaterial color={color} toneMapped={false} />
             </mesh>
@@ -28,7 +25,7 @@ function Rig({ children }) {
     const { camera, mouse } = useThree();
 
     useFrame(() => {
-        camera.position.lerp(vec.set(mouse.x * 2, 0, 3.5), 0.05);
+        camera.position.lerp(vec.set(mouse.x * 2, 0, 10.5), 0.05);
         ref.current.position.lerp(vec.set(mouse.x, mouse.y * 0.1, 0), 0.1);
         ref.current.rotation.y = THREE.MathUtils.lerp(ref.current.rotation.y, (-mouse.x * Math.PI) / 20, 0.1);
     });
@@ -37,19 +34,35 @@ function Rig({ children }) {
 }
 
 function Ground(props) {
-    const [floor, normal] = useTexture([
-        '/SurfaceImperfections003_1K_var1.jpg',
-        '/SurfaceImperfections003_1K_Normal.jpg',
+    // const [roughnessMap, normalMap] = useTexture([
+    //     '/SurfaceImperfections003_1K_var1.jpg',
+    //     '/SurfaceImperfections003_1K_Normal.jpg',
+    // ]);
+    const [roughnessMap, normalMap] = useTexture([
+        '/Surface_Imperfections_Stains_001_SD/Surface_Imperfections_Stains_001_roughness.jpg',
+        '/Surface_Imperfections_Stains_001_SD/Surface_Imperfections_Stains_001_normal.jpg',
     ]);
+
     return (
-        <Reflector resolution={1024} args={[8, 8]} {...props}>
+        <Reflector
+            resolution={1024}
+            args={[20, 15]}
+            mirror={0.3}
+            mixBlur={5}
+            mixStrength={1}
+            rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+            {...props}
+        >
             {(Material, props) => (
                 <Material
-                    color="#f0f0f0"
-                    metalness={0}
-                    roughnessMap={floor}
-                    normalMap={normal}
-                    normalScale={[0.6, 0.6]}
+                    metalness={0.3}
+                    // map={map}
+                    color="#888"
+                    roughnessMap={roughnessMap}
+                    normalMap={normalMap}
+                    // normalScale={[1, 1]}
+                    // displacementMap={displacementMap}
+
                     {...props}
                 />
             )}
@@ -57,33 +70,97 @@ function Ground(props) {
     );
 }
 
+function AnimatedBloom({
+    lights = [],
+    meshes = [],
+}: {
+    lights?: MutableRefObject<THREE.Light>[];
+    meshes?: MutableRefObject<THREE.Mesh>[];
+}) {
+    const bloom1 = useRef();
+    const bloom2 = useRef();
+
+    useFrame((_) => {
+        const value = Math.abs(Math.sin(_.clock.elapsedTime * 2));
+        bloom1.current.intensity = 0.3 + value;
+        bloom2.current.intensity = 0.35 + value;
+    });
+
+    return (
+        <>
+            <SelectiveBloom
+                ref={bloom1}
+                lights={lights}
+                selection={meshes}
+                selectionLayer={10}
+                intensity={0.65}
+                blurPass={undefined}
+                width={Resizer.AUTO_SIZE}
+                height={Resizer.AUTO_SIZE}
+                kernelSize={KernelSize.HUGE}
+                luminanceThreshold={0}
+                luminanceSmoothing={0}
+            />
+            <SelectiveBloom
+                ref={bloom2}
+                lights={lights}
+                selection={meshes}
+                selectionLayer={10}
+                intensity={0.6}
+                blurPass={undefined}
+                width={Resizer.AUTO_SIZE}
+                height={Resizer.AUTO_SIZE}
+                kernelSize={3}
+                luminanceThreshold={0}
+                luminanceSmoothing={0.4}
+            />
+        </>
+    );
+}
+
 export default function ReflectorFloorPage() {
+    const ambientLight = useRef<THREE.AmbientLight>();
+    const spotLight = useRef<THREE.SpotLight>();
+    const wall = useRef<THREE.Mesh>();
+
     return (
         <DefaultLayout documentTitle="Reflector floor">
-            <Canvas dpr={[1, 1.5]} camera={{ position: [0, 0, 15] }}>
+            <Canvas
+                dpr={[1, 2]}
+                gl={{
+                    alpha: false,
+                    powerPreference: 'high-performance',
+                    antialias: false,
+                    stencil: false,
+                    depth: false,
+                }}
+                camera={{ position: [0, 0, 10], fov: 30 }}
+            >
                 <color attach="background" args={['black']} />
-                <ambientLight />
-                {/* <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} /> */}
+                <fog attach="fog" args={['black', 12, 20]} />
+                <ambientLight ref={ambientLight} intensity={1} />
+                <spotLight ref={spotLight} position={[0, 10, 0]} intensity={0.35} />
+                {/* <directionalLight position={[-20, 0, -10]} intensity={0.7} /> */}
+                {/* <pointLight intensity={0.35} position={[2, 10, 5]} /> */}
+                {/* <OrbitControls enableZoom={true} enablePan={false} enableRotate={true} /> */}
                 <Suspense fallback={null}>
-                    <Rig>
-                        <Triangle color="#ff2060" scale={0.009} rotation={[0, 0, Math.PI / 3]} />
-                        <Triangle color="cyan" scale={0.009} position={[2, 0, -2]} rotation={[0, 0, Math.PI / 3]} />
-                        <Triangle color="orange" scale={0.009} position={[-2, 0, -2]} rotation={[0, 0, Math.PI / 3]} />
-                        <Triangle color="white" scale={0.009} position={[0, 2, -10]} rotation={[0, 0, Math.PI / 3]} />
-                        <Ground
-                            mirror={1}
-                            mixBlur={3}
-                            mixStrength={1}
-                            rotation={[-Math.PI / 2, 0, Math.PI / 2]}
-                            position-y={-0.8}
-                        />
-                    </Rig>
-                    <EffectComposer multisampling={8}>
-                        <Bloom kernelSize={5} luminanceThreshold={0} luminanceSmoothing={0.4} intensity={0.6} />
-                        {/* <Bloom kernelSize={5} luminanceThreshold={0} luminanceSmoothing={0} intensity={0.4} /> */}
+                    {/* <Rig> */}
+                    {/* <ChipsaLogo color="orange" scale={0.1} position={[-1.5, 0, -1]} /> */}
+                    <mesh ref={wall} position={[0, 0.001, -3]}>
+                        <planeBufferGeometry args={[5, 5, 1, 1]} />
+                        <meshStandardMaterial metalness={0} color="orange" />
+                    </mesh>
+                    <mesh position={[1, 0.001, 5]}>
+                        <planeBufferGeometry args={[1, 1, 1, 1]} />
+                        <meshStandardMaterial metalness={0} color="red" />
+                    </mesh>
+                    <Ground position-y={-0.8} />
+                    <EffectComposer frameBufferType={THREE.HalfFloatType}>
+                        <SMAA />
+                        <AnimatedBloom lights={[ambientLight, spotLight]} meshes={[wall]} />
                     </EffectComposer>
                 </Suspense>
-                <CameraShake yawFrequency={0.2} pitchFrequency={0.2} rollFrequency={0.2} />
+                <CameraShake yawFrequency={0.2} pitchFrequency={0.2} rollFrequency={0.2} intensity={0.5} />
             </Canvas>
         </DefaultLayout>
     );
