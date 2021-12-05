32426a76a0e9fad0999d09f837d289a94a4f1b86
Pavel Mazhuga <pavelmazhuga@yandex.ru>
2021-12-05 14:51:21 +0700

    .

--- pages/reflections/reflector-floor.tsx
+++ pages/reflections/reflector-floor.tsx
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { useRef, Suspense, useMemo, MutableRefObject, useEffect, forwardRef } from 'react';
 import { Canvas, useThree, useFrame, useLoader } from '@react-three/fiber';
-import { Reflector, CameraShake, OrbitControls, useTexture } from '@react-three/drei';
+import { Reflector, CameraShake, OrbitControls, useTexture, Stats } from '@react-three/drei';
 import { EffectComposer, SMAA, SelectiveBloom, Bloom } from '@react-three/postprocessing';
 import { BlurPass, Resizer, KernelSize } from 'postprocessing';
 import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
@@ -60,24 +60,14 @@ function Ground(props) {
         <Reflector
             resolution={1024}
             args={[20, 15]}
-            mirror={0.3}
+            mirror={0.2}
             mixBlur={5}
-            mixStrength={1}
+            mixStrength={0.9}
             rotation={[-Math.PI / 2, 0, Math.PI / 2]}
             {...props}
         >
             {(Material, props) => (
-                <Material
-                    metalness={0.3}
-                    // map={map}
-                    color="#888"
-                    roughnessMap={roughnessMap}
-                    normalMap={normalMap}
-                    // normalScale={[1, 1]}
-                    // displacementMap={displacementMap}
-
-                    {...props}
-                />
+                <Material metalness={0} color="#888" roughnessMap={roughnessMap} normalMap={normalMap} {...props} />
             )}
         </Reflector>
     );
@@ -91,7 +81,7 @@ function AnimatedBloom({
     meshes?: MutableRefObject<THREE.Mesh>[];
 }) {
     const bloom1 = useRef();
-    const bloom2 = useRef();
+    // const bloom2 = useRef();
 
     useFrame((_) => {
         const value = Math.abs(Math.sin(_.clock.elapsedTime * 2));
@@ -114,19 +104,6 @@ function AnimatedBloom({
                 luminanceThreshold={0}
                 luminanceSmoothing={0}
             />
-            {/* <SelectiveBloom
-                ref={bloom2}
-                lights={lights}
-                selection={meshes}
-                selectionLayer={10}
-                intensity={0.6}
-                blurPass={undefined}
-                width={Resizer.AUTO_SIZE}
-                height={Resizer.AUTO_SIZE}
-                kernelSize={3}
-                luminanceThreshold={0}
-                luminanceSmoothing={0.4}
-            /> */}
         </>
     );
 }
@@ -160,12 +137,12 @@ export default function ReflectorFloorPage() {
                 {/* <OrbitControls enableZoom={true} enablePan={false} enableRotate={true} /> */}
                 <Suspense fallback={null}>
                     <Rig>
-                        <group position={[0, 0.001, -3]}>
+                        <group position={[0, 0.6, -3]}>
                             <mesh ref={wall}>
-                                <planeBufferGeometry args={[5, 5, 1, 1]} />
+                                <planeBufferGeometry args={[5, 3, 1, 1]} />
                                 <meshStandardMaterial metalness={0} color="#FCCF69" />
                             </mesh>
-                            <ChipsaLogo ref={chipsa} color="black" scale={0.1} position={[-1.5, 0.2, 0.002]} />
+                            <ChipsaLogo ref={chipsa} color="black" scale={0.1} position={[-1.5, -0.5, 0.002]} />
                         </group>
                         <Ground position-y={-0.8} />
                     </Rig>
@@ -175,6 +152,7 @@ export default function ReflectorFloorPage() {
                     </EffectComposer>
                 </Suspense>
                 <CameraShake yawFrequency={0.2} pitchFrequency={0.2} rollFrequency={0.2} intensity={0.5} />
+                {/* <Stats /> */}
             </Canvas>
         </DefaultLayout>
     );
